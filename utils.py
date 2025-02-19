d04ea1818fb45be0d218ef1422cf1ae2b764b7b4
gkianfar <gkianfarproject@gmail.com>
2025-02-19 11:55:40 +0000

    combined dataset added to preprocessing.py

--- utils.py
+++ utils.py
@@ -197,3 +197,163 @@ def get_impossible_count(activity_df,transition_matrix,threshold,occurence_thres
       if not impossible_activity_count[p]:
         del impossible_activity_count[p]
   return impossible_activity_count
+
+class AGITATION_DATASET():
+  def __init__(self,dataset, location_names, physiology_names):
+    num_samples = len(dataset)
+    self.patient_id = [dataset[i][0] for i in range(num_samples) ]
+    self.start_time = [dataset[i][1] for i in range(num_samples) ]
+    self.end_time = [dataset[i][2] for i in range(num_samples) ]
+    self.activity = [dataset[i][3] for i in range(num_samples) ]
+    self.non_agitation = [dataset[i][4] for i in range(num_samples) ]
+    self.agitation_params = [dataset[i][5] for i in range(num_samples) ]
+    self.age = [dataset[i][6] for i in range(num_samples) ]
+    self.gender = [dataset[i][7] for i in range(num_samples) ]
+    self.week_day = [dataset[i][8] for i in range(num_samples) ]
+
+    # Initiate the dataframe with basic informations
+    self.init_df()
+    self.location_names = location_names
+    self.location_columns = []
+    self.location_columns.extend(self.location_names)
+    relative_cols = []
+    mean_cols = []
+    std_cols = []
+    for location in self.location_names:
+      relative_cols.append(location+'_count_relative')
+      mean_cols.append('normal_'+location+'_count_mean')
+      std_cols.append('normal_'+location+'_count_std')
+    self.location_columns.extend(relative_cols)
+    self.location_columns.extend(mean_cols)
+    self.location_columns.extend(std_cols)
+    self.physiology_names = physiology_names
+  def __len__(self):
+    return len(self.patient_id)
+  def init_df(self):
+    self.feature_df = pd.DataFrame(np.column_stack((self.patient_id,
+                      self.start_time, self.end_time, self.week_day, self.age, self.gender)),
+                       columns=['patient_id', 'start_time', 'end_time', 'week_day','age', 'gender'])
+  def activity_change(self, inplace = True):
+    activity_change = []
+    for index in range(self.__len__()):
+      dummy = self.activity[index]
+      dummy = np.array(dummy)
+      result = np.concatenate([[np.NaN], dummy[1:] != dummy[:-1]])
+      changed = np.nansum(result)
+
+      normal_activity_changes = []
+      count_sample = 0
+      normal_activity_samples = len(self.non_agitation[index])
+
+      for arr in self.non_agitation[index]:
+        arr = np.array(arr)
+        if len(arr)==0:
+          continue
+        result = arr[1:] != arr[:-1]
+        count_sample+=1
+        normal_activity_changes.append(np.nansum(result))
+
+      # Calculate sample mean and standard deviation
+      normal_activity_change_mean = np.mean(normal_activity_changes)
+      normal_activity_change_std = np.std(normal_activity_changes)
+      relative_change = (changed - normal_activity_change_mean)/normal_activity_change_std
+      if not relative_change:
+        relative_change = 0
+      activity_change.append([changed, relative_change, normal_activity_change_mean, normal_activity_change_std,normal_activity_samples])
+      activity_change_df = pd.DataFrame(activity_change, columns=['change_count', 'change_relative', 'normal_change_mean','normal_change_std','normal_samples'])
+    if inplace:
+      self.feature_df = pd.concat([self.feature_df,activity_change_df],axis=1)
+    else:
+      return activity_change_df
+  def activity_count(self, inplace = True):
+    activity_count = []
+    for index in range(self.__len__()):
+      dummy = self.activity[index]
+      dummy = np.array(dummy)
+      count_df = pd.Series(dummy).value_counts()
+      #print(count_df)
+      result = []
+      for location in self.location_names:
+        if location in count_df.index:
+          result.append(count_df.loc[location])
+        else:
+          result.append(0)
+
+
+      normal_activity_counts = []
+      for arr in self.non_agitation[index]:
+        arr = np.array(arr)
+        if len(arr)==0:
+          continue
+        count_df = pd.Series(arr).value_counts()
+        result = []
+        for location in self.location_names:
+          if location in count_df.index:
+            result.append(count_df.loc[location])
+          else:
+            result.append(0)
+
+        normal_activity_counts.append(result)
+      normal_activity_counts = np.array(normal_activity_counts)
+      activity_count_mean = np.mean(normal_activity_counts, axis=0)
+      activity_count_std = np.std(normal_activity_counts, axis=0)
+      activity_count_relative = (np.array(result) - activity_count_mean)/activity_count_std
+      activity_count_relative = np.nan_to_num(activity_count_relative, nan=0)
+      #print(f'act rel:{activity_count_relative}')
+
+      #print(f'rel {activity_count_relative.shape}')
+      #print(f'mean {activity_count_mean.shape}')
+      #print(f'std {activity_count_std.shape}')
+      #print(f'result {len(result)}')
+
+      activity_count.append(np.concatenate([result,activity_count_relative, activity_count_mean,activity_count_std]))
+
+
+    activity_count_df = pd.DataFrame(activity_count,
+              columns=self.location_columns)
+
+    if inplace:
+      self.feature_df = pd.concat([self.feature_df,activity_count_df],axis=1)
+    else:
+      return activity_count_df
+
+  def feature_extraction(self, inplace = True):
+    self.init_df()
+    self.activity_change(inplace)
+    self.activity_count(inplace)
+    self.physiology_features(inplace)
+    if inplace:
+      print('Feature dataframe got updated!')
+    else:
+      return self.feature_df
+
+  def physiology_features(self, inplace = True):
+    physiology_features = []
+    for index in range(self.__len__()):
+      dummy = self.agitation_params[index]
+      dict_dummy = dict(dummy)
+
+      result = []
+      for param in self.physiology_names:
+        if param in dict_dummy.keys():
+          result.append(dict_dummy[param])
+        else:
+          result.append(0)
+      physiology_features.append(result)
+      physiology_df = pd.DataFrame(physiology_features,columns=self.physiology_names)
+
+    if inplace:
+      self.feature_df = pd.concat([self.feature_df,physiology_df],axis=1)
+    else:
+      return physiology_df
+  def set_label(self,label):
+    self.feature_df['label'] = label
+
+  def save_df(self,path):
+    self.feature_df.to_csv(path)
+
+def save_concat_datasets(df_list,path):
+  df_list = pd.concat(df_list)
+  df_list.to_csv(path)
+
+  
